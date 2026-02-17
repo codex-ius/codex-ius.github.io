f9c31838f4a8b2af3b1cf7f520c46605bb12b10f
codex-ius <rodrigo.dev.ius@gmail.com>
2026-02-17 18:57:42 -0300

    Create data.js

---
+++ js/utils/data.js
@@ -0,0 +1,63 @@
+// js/utils/data.js
+export const cardsData = {
+  ensayos: [
+    {
+      title: 'Teoría de la atribución personal (TAPCEE)',
+      description: 'Técnica original para investigar tipos delictivos. Síntesis de investigación 2017.',
+      link: 'https://drive.google.com/file/d/1w8NlkY3s4ycnHOV231op3DllBw9CYlOD/view',
+      linkText: 'Abrir PDF',
+      branch: 'penal',
+      author: 'Abog. Quiroga, R.',
+      date: '2018'
+    },
+    {
+      title: 'Crítica al Sistema Penal',
+      description: 'Análisis estadístico sobre la impunidad y gestión de la estadística criminal.',
+      link: 'https://drive.google.com/file/d/1p0uVjgE-qW5_wzlft0q4Y7T3X5UhWN9m/view',
+      linkText: 'Abrir PDF',
+      branch: 'penal',
+      author: 'Abog. Quiroga, R.',
+      date: '2021'
+    }
+  ],
+  educacion: [
+    {
+      title: 'Introducción al Derecho',
+      description: 'Cápsulas y guías de estudio fundamentales.',
+      link: 'sources/cursos/introduccion_derecho.html',
+      linkText: 'Ir al curso',
+      branch: 'Fundamentos',
+      level: 'Inicial',
+      time: '2 horas'
+    }
+  ],
+  noticias: [
+    {
+      title: 'Refactorización Codex_ius 2026',
+      description: 'Migración a arquitectura Glass-Organic y optimización de accesibilidad.',
+      link: '#',
+      linkText: 'Leer más',
+      branch: 'SISTEMA',
+      date: '17/02/2026'
+    }
+  ],
+  software: [
+    {
+      title: 'Calculadora Laboral',
+      description: 'Liquidación de rubros indemnizatorios (Ley 20.744).',
+      link: 'sources/apps/calculadora-laboral.html',
+      linkText: 'Ejecutar',
+      icon: 'calculate',
+      version: 'v2.0'
+    },
+    {
+      title: 'Editor de Codex_ius',
+      description: 'Herramienta editorial para la generación de contenido jurídico.',
+      link: 'sources/apps/editorial_codexius.html',
+      linkText: 'Abrir Editor',
+      icon: 'edit_note',
+      version: 'v1.5'
+    }
+  ]
+};
+        
